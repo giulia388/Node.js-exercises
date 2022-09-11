5965dca76c97a9d365435bc1287f47340b8dee24
Giulia Colucci <giuliacolucci388@gmail.com>
2022-09-11 21:14:50 +0200

    add exercise 5

--- Exercise-5/server.mjs
+++ Exercise-5/server.mjs
@@ -8,14 +8,11 @@ const server = createServer((request, response) => {
     response.setHeader("Content-Type", "text/html");
 
     response.end(
-        `<html>
-            <body>
-                <h1> This page was served with Node.js </h1>
-            </body>
-        </html>`
+        "<html><body><h1>This page was served with Node.js!</h1></body></html>"
     );
 });
 
 server.listen(3000, () => {
     console.log(`Server running at http://localhost:3000`);
+    //ctrl c stops the server run
 });
