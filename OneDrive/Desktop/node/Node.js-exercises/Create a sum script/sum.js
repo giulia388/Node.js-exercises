a85440480df72f673b2aa7e158aed12a3d6b1005
Giulia Colucci <giuliacolucci388@gmail.com>
2022-09-13 16:17:50 +0200

    add exercise

--- OneDrive/Desktop/node/Node.js-exercises/Create a sum script/sum.js
+++
@@ -1,7 +0,0 @@
-function sum(num1, num2) {
-    return num1+num2
-}
-
-const total = sum(5, 6);
-
-console.log(total)
--- OneDrive/Desktop/node/Node.js-exercises/Exercise-1/sum.js
+++ OneDrive/Desktop/node/Node.js-exercises/Exercise-1/sum.js
@@ -1,5 +1,5 @@
 function sum(num1, num2) {
-    return num1 + num2
+    return num1+num2
 }
 
 const total = sum(5, 6);
